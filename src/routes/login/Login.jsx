06bab5605b037edf2eb831b7cb8c03353b1818a0
CecemelDev <cedricthauvoye1@gmail.com>
2022-01-22 13:42:54 +0100

    Added Login page

--- src/routes/login/Login.jsx
+++ src/routes/login/Login.jsx
@@ -1,6 +1,17 @@
+import NavBar from "../../components/navbar/NavBar.jsx"
+import "./_login.scss";
+
 function Login() {
     return (
-        <>Login</>
+        <>
+            <NavBar />
+            <h1 className="title">Login</h1>
+            <form className="loginForm" action="">
+                <input className="loginTextInput" type="text" placeholder="Username"/>
+                <input className="loginTextInput" type="text" placeholder="Password" />
+                <button className="loginButtonInput" type="submit">Login</button>
+            </form>
+        </>
     );
 }
 
