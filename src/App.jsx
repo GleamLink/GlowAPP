c98aa0a5cd24424dba35d696f2af914fbff584d3
CecemelDev <cedricthauvoye1@gmail.com>
2022-01-29 15:38:37 +0100

    Changed to react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
 import "./_app.scss"
 
-import Home from "./routes/home/Home";
-import Login from "./routes/login/Login";
-import Friends from "./routes/friends/Friends";
-import Posts from "./routes/posts/Posts";
-import Communities from "./routes/communities/Communities";
-import Tunes from "./routes/tunes/Tunes";
+import { lazy, Suspense } from "preact/compat";
 
-import Router from 'preact-router';
+const Home = lazy(() => import("./routes/home/Home"));
+const Login = lazy(() => import("./routes/login/Login"));
+const Friends = lazy(() => import("./routes/friends/Friends"));
+const Posts = lazy(() => import("./routes/posts/Posts"));
+const Communities = lazy(() => import("./routes/communities/Communities"));
+const Tunes = lazy(() => import("./routes/tunes/Tunes"));
+
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 import { CookiesProvider } from 'react-cookie';
 
 export function App(props) {
   return (
-    <CookiesProvider>
       <Router>
-        <Home path="/" />
-        <Login path="/login" />
-        <Friends path="/friends" />
-        <Posts path="/posts" />
-        <Communities path="/communities" />
-        <Tunes path="/tunes" />
+        <Suspense>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/friends" component={Friends} />
+            <Route exact path="/posts" component={Posts} />
+            <Route exact path="/communities" component={Communities} />
+            <Route exact path="/tunes" component={Tunes} />
+          </Switch>
+        </Suspense>
+        
       </Router>
-    </CookiesProvider>
+    
   )
 }
