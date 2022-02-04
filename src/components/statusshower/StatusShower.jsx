59481db6fed5e613d0cea3b9dfbe42b60d36bdb8
CecemelDev <cedricthauvoye1@gmail.com>
2022-02-05 00:53:04 +0100

    Added 2 sizes StatusShower

--- src/components/statusshower/StatusShower.jsx
+++ src/components/statusshower/StatusShower.jsx
@@ -2,10 +2,20 @@ import "./_statusshower.scss"
 
 function StatusShower(props) {
     const status = props.status
-    if(status == "online")
-        return (
-            <div className="onlineStatus" />
-        );
+    const big = props.big
+    if(status == "online") {
+        if(big) {
+            return (
+                <div className="onlineStatus" style={`width: 17px; height: 17px`} />
+            )
+        } 
+        else
+        {
+            return (
+                <div className="onlineStatus" style={`width: 10px; height: 10px`} />
+            )
+        }
+    }
     else if(status == "offline")
         return (
             <div className="offlineStatus" />
