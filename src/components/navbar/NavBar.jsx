340c6e7b073fd462133bb9c3716cb2b9824e7f67
CecemelDev <cedricthauvoye1@gmail.com>
2022-02-06 17:48:31 +0100

    Added responsive Navbar for phone

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -10,12 +10,18 @@ import HomeIcon from '@mui/icons-material/Home';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LogoutIcon from '@mui/icons-material/Logout';
 
+import DehazeIcon from '@mui/icons-material/Dehaze';
+
 import react from "react"
 import { getToken, removeUserSession } from "../../Utils/Common";
-import { useEffect } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
+import { Divider, Drawer, IconButton, SwipeableDrawer } from "@mui/material";
+import { ChevronLeft } from "@mui/icons-material";
 
 
 function NavBar() {
+
+    const [isResponsiveNavbarOpen, setNavbarOpen] = useState(false)
 
     const handleLogout = () => {
         removeUserSession()
@@ -25,56 +31,127 @@ function NavBar() {
         <>
             <div className="navBar">
                 <ul className="navbarList">
-                    <li>
-                        <a href="/" className="navbarListItem">
-                            <HomeIcon className="icon"/>
-                            <span>Home</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/friends" className="navbarListItem">
-                            <PeopleAltIcon className="icon"/>
-                            <span>Friends</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/posts" className="navbarListItem">
-                            <PhotoLibraryIcon className="icon"/>
-                            <span>Posts</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/communities" className="navbarListItem">
-                            <PeopleIcon className="icon"/>
-                            <span>Communities</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/tunes" className="navbarListItem">
-                            <PlayCircleFilledIcon className="icon"/>
-                            <span>Tunes</span>
-                        </a>
-                    </li>
-                    {sessionStorage.getItem('token') ? (
-                        <div style={{ "margin-left": "auto", "display": "flex", "flex-direction": "row" }}>
-                            <li>
-                                <a href="/profile" className="navbarListItem" >
-                                    <AccountCircleIcon className="icon"/>
-                                    <span>Profile</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a className="navbarListItem" onClick={handleLogout} >
-                                    <LogoutIcon className="icon"/>
-                                    <span>Logout</span>
-                                </a>
-                            </li>
-                        </div>
+                    <div className="computer">
+                        <li>
+                            <a href="/" className="navbarListItem">
+                                <HomeIcon className="icon"/>
+                                <span>Home</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/friends" className="navbarListItem">
+                                <PeopleAltIcon className="icon"/>
+                                <span>Friends</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/posts" className="navbarListItem">
+                                <PhotoLibraryIcon className="icon"/>
+                                <span>Posts</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/communities" className="navbarListItem">
+                                <PeopleIcon className="icon"/>
+                                <span>Communities</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/tunes" className="navbarListItem">
+                                <PlayCircleFilledIcon className="icon"/>
+                                <span>Tunes</span>
+                            </a>
+                        </li>
+                        {sessionStorage.getItem('token') ? (
+                            <div style={{ "margin-left": "auto", "display": "flex", "flex-direction": "row" }}>
+                                <li>
+                                    <a href="/profile" className="navbarListItem" >
+                                        <AccountCircleIcon className="icon"/>
+                                        <span>Profile</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="navbarListItem" onClick={handleLogout} >
+                                        <LogoutIcon className="icon"/>
+                                        <span>Logout</span>
+                                    </a>
+                                </li>
+                            </div>
+                                
                             
-                        
-                    ): (<></>)}
+                        ): (<></>)}
+                    </div>
+                    <div className="smartphone">
+                        <li>
+                            <a onClick={() => setNavbarOpen(true)} className="navbarResponsiveMenu">
+                                <DehazeIcon className="icon" />
+                            </a>
+                        </li>
+                        <Drawer
+                            sx={{
+                                flexShrink: 0,
+                            }}
+                            variant="persistent"
+                            anchor="left"
+                            open={isResponsiveNavbarOpen}
+                            closeAfterTransition={true}
+                        >
+                            <IconButton onClick={() => setNavbarOpen(false)}>
+                                <ChevronLeft />
+                            </IconButton>
+                            <li>
+                                <a href="/" className="navbarListItem">
+                                    <HomeIcon className="icon"/>
+                                    <span>Home</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/friends" className="navbarListItem">
+                                    <PeopleAltIcon className="icon"/>
+                                    <span>Friends</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/posts" className="navbarListItem">
+                                    <PhotoLibraryIcon className="icon"/>
+                                    <span>Posts</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/communities" className="navbarListItem">
+                                    <PeopleIcon className="icon"/>
+                                    <span>Communities</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/tunes" className="navbarListItem">
+                                    <PlayCircleFilledIcon className="icon"/>
+                                    <span>Tunes</span>
+                                </a>
+                            </li>
+                            <Divider />
+                            {sessionStorage.getItem('token') ? (
+                                <div style={{ "margin-left": "auto", "display": "flex", "flex-direction": "row" }}>
+                                    <li>
+                                        <a href="/profile" className="navbarListItem" >
+                                            <AccountCircleIcon className="icon"/>
+                                            <span>Profile</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className="navbarListItem" onClick={handleLogout} >
+                                            <LogoutIcon className="icon"/>
+                                            <span>Logout</span>
+                                        </a>
+                                    </li>
+                                </div>
+                            ): (<></>)}
+                        </Drawer>
+                    </div>
+                    
                     
                 </ul>
+                
             </div>
         </>
         
